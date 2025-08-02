8fadf49822b34a768436b3540808b395f6d59896
akhila1534 <akhilachandrakkantha@gmail.com>
2025-08-02 14:08:41 +1200

    creating diff versions of my wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,30 +1,36 @@
 //your parameter variables go here!
+
 let rect_width  = 20;
 let rect_height = 30;
-let dpetal1 = 35
-let dflowercenter1 = 30 
-let dpetal2 = 25
-let dflowercenter2 = 20
-let dpetal3 = 28
-let dflowercenter3 = 25
-let dpetal4 = 15
-let dflowercenter4 = 14
-let dpetal5 = 15
-let dflowercenter5 = 12
-let dpetal6 = 13 
-let dflowercenter6 = 11
+
+let dpetal1 = 80
+let dflowercenter1 = 60
+
+let dpetal2 = 50
+let dflowercenter2 = 30
+
+let dpetal3 = 70
+let dflowercenter3 = 40
+
+let dpetal4 = 40
+let dflowercenter4 = 20
+
+let dpetal5 = 30
+let dflowercenter5 = 20
+
+let dpetal6 = 30
+let dflowercenter6 = 20
+
 let rectangleX = 0
 let rectangleW = 200
 let rectangleH = 30
-let switcher = true;
 
-
-
+let switcher = false;
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -35,144 +41,147 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   background(214,255,238); //light honeydew green colour
+
+
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+ 
+function my_symbol() {
   //rect(40 ,40, rect_width, rect_height);
-//og d petal 35 
+  //og d petal 35 
 
 
-if (switcher == true) {
-  rectangle();
+  if (switcher == true) {
+    rectangle();
 
-}
+  }
   noStroke(); // no outline 
+
+
 
   //FLOWER 1 
   //shadow
-  fill(196, 26, 139)
-  circle (64,89, dpetal1)
-  circle (76,63, dpetal1)
-  circle (106,58, dpetal1)
-  circle (83,110, dpetal1)
+  fill(196, 26, 139);
+  circle(64, 89, dpetal1);
+  circle(76, 63, dpetal1);
+  circle(106, 58, dpetal1);
+  circle(83, 110, dpetal1);
 
   //petals 
-  fill(255,39,183)
-  circle (70, 90, dpetal1)
-  circle (80, 65, dpetal1)
-  circle (110, 60, dpetal1)
-  circle (125, 85, dpetal1)
-  circle (115, 110, dpetal1)
-  circle (87, 110, dpetal1)
+  fill(255, 39, 183);
+  circle(70, 90, dpetal1);
+  circle(80, 65, dpetal1);
+  circle(110, 60, dpetal1);
+  circle(125, 85, dpetal1);
+  circle(115, 110, dpetal1);
+  circle(87, 110, dpetal1);
 
   //center of flower
-  fill(255, 245, 133)
-  circle (100, 85, dflowercenter1) 
+  fill(255, 245, 133);
+  circle(100, 85, dflowercenter1);
 
-// FLOWER 2 
-//shadow 
-fill (214, 123, 26)
-circle (156, 9, dpetal2)
-circle (137, 19, dpetal2)
-circle (142, 39, dpetal2)
-//petals 
-fill(255,147,31)
-circle (160, 10, dpetal2)
-circle (140, 20, dpetal2)
-circle (145, 40, dpetal2)
-circle (165, 45, dpetal2)
-circle (175, 26, dpetal2)
+  // FLOWER 2 
+  //shadow 
+  fill(214, 123, 26);
+  circle(156, 9, dpetal2);
+  circle(137, 19, dpetal2);
+  circle(142, 39, dpetal2);
+  //petals 
+  fill(255, 147, 31);
+  circle(160, 10, dpetal2);
+  circle(140, 20, dpetal2);
+  circle(145, 40, dpetal2);
+  circle(165, 45, dpetal2);
+  circle(175, 26, dpetal2);
 
-//center 
-fill(97,227,255)
-circle (158, 28, dflowercenter2)
- 
-//FLOWER 3 
-//shadow
-fill (152, 191, 46)
-circle (135, 154, dpetal3)
-circle (146, 174, dpetal3)
-circle (147, 134, dpetal3)
-circle (170, 134, dpetal3)
+  //center 
+  fill(97, 227, 255);
+  circle(158, 28, dflowercenter2);
 
-//petals
-fill (188,234,58)
-circle (140, 155, dpetal3)
-circle (152, 175, dpetal3)
-circle (175, 175, dpetal3)
-circle (185, 155, dpetal3)
-circle (152, 135, dpetal3)
-circle (175, 135, dpetal3)
+  //FLOWER 3 
+  //shadow
+  fill(152, 191, 46);
+  circle(135, 154, dpetal3);
+  circle(146, 174, dpetal3);
+  circle(147, 134, dpetal3);
+  circle(170, 134, dpetal3);
 
-//center 
-fill (255,77,7)
-circle (164, 155,dflowercenter3)
+  //petals
+  fill(188, 234, 58);
+  circle(140, 155, dpetal3);
+  circle(152, 175, dpetal3);
+  circle(175, 175, dpetal3);
+  circle(185, 155, dpetal3);
+  circle(152, 135, dpetal3);
+  circle(175, 135, dpetal3);
 
-//FLOWER 4 
-//shadow 
-fill (179, 35, 30)
-circle (27, 164, dpetal4)
-circle (32, 174, dpetal4)
-circle (32, 156, dpetal4)
-circle (43, 156, dpetal4)
+  //center 
+  fill(255, 77, 7);
+  circle(164, 155, dflowercenter3);
 
-//petals
-fill (255,77,77)
-circle (30, 165, dpetal4)
-circle (35, 175, dpetal4)
-circle (45, 175, dpetal4)
-circle (50, 165, dpetal4)
-circle (45, 157, dpetal4)
-circle (35, 157, dpetal4)
+  //FLOWER 4 
+  //shadow 
+  fill(179, 35, 30);
+  circle(27, 164, dpetal4);
+  circle(32, 174, dpetal4);
+  circle(32, 156, dpetal4);
+  circle(43, 156, dpetal4);
 
-//center 
-fill (188,234,58)
-circle (40, 165, dflowercenter4)
+  //petals
+  fill(255, 77, 77);
+  circle(30, 165, dpetal4);
+  circle(35, 175, dpetal4);
+  circle(45, 175, dpetal4);
+  circle(50, 165, dpetal4);
+  circle(45, 157, dpetal4);
+  circle(35, 157, dpetal4);
 
-//FLOWER 5 
-//shadow 
-fill (96, 162, 168)
-circle (25, 24, dpetal5)
-circle (31, 14, dpetal5)
-circle (43, 14, dpetal5)
-circle (30, 34, dpetal5)
+  //center 
+  fill(188, 234, 58);
+  circle(40, 165, dflowercenter4);
 
-//petals
-fill (97,227,255)
-circle (27,25,dpetal5)
-circle (33,15,dpetal5)
-circle (45,15,dpetal5)
-circle (50,25,dpetal5)
-circle (33,35,dpetal5)
-circle (45,35,dpetal5)
+  //FLOWER 5 
+  //shadow 
+  fill(96, 162, 168);
+  circle(25, 24, dpetal5);
+  circle(31, 14, dpetal5);
+  circle(43, 14, dpetal5);
+  circle(30, 34, dpetal5);
 
-//center 
-fill (255,151,80)
-circle (39,25, dflowercenter5)
+  //petals
+  fill(97, 227, 255);
+  circle(27, 25, dpetal5);
+  circle(33, 15, dpetal5);
+  circle(45, 15, dpetal5);
+  circle(50, 25, dpetal5);
+  circle(33, 35, dpetal5);
+  circle(45, 35, dpetal5);
 
-//FLOWER 6 
-//shadow 
-fill (95, 39, 143)
-circle (-3,109,dpetal6)
-circle (-3,120,dpetal6)
-circle (8,103,dpetal6)
-circle (7,124,dpetal6)
+  //center 
+  fill(255, 151, 80);
+  circle(39, 25, dflowercenter5);
 
-//petals 
-fill (141,56,214)
-circle (0,110,dpetal6)
-circle (10,105,dpetal6)
-circle (18,110,dpetal6)
-circle (18,120,dpetal6)
-circle (10,125,dpetal6)
-circle (0,120,dpetal6)
+  //FLOWER 6 
+  //shadow 
+  fill(95, 39, 143);
+  circle(-3, 109, dpetal6);
+  circle(-3, 120, dpetal6);
+  circle(8, 103, dpetal6);
+  circle(7, 124, dpetal6);
 
-//center
-fill (255,237,57)
-circle (9,115,dflowercenter6)
+  // //petals 
+  fill(141, 56, 214);
+  circle(0, 110, dpetal6);
+  circle(10, 105, dpetal6);
+  circle(18, 110, dpetal6);
+  circle(18, 120, dpetal6);
+  circle(10, 125, dpetal6);
+  circle(0, 120, dpetal6);
 
-
-}
+  // //center
+  fill(255, 237, 57);
+  circle(9, 115, dflowercenter6);
+  }
 
 function rectangle() {
   noStroke();
@@ -184,34 +193,3 @@ function rectangle() {
 
 
 }
-
-//flower 5 petals 
-// circle (7,20, dpetal5)
-// circle (12, 10, dpetal5)
-// circle (23, 10, dpetal5)
-// circle (27,20, dpetal5)
-// circle (22, 28, dpetal5)
-// circle (13, 28, dpetal5)
-
-//new flower 5 
-// circle (18, 35, dpetal5)
-// circle (27, 20, dpetal5)
-// circle (45, 20, dpetal5)
-// circle (52, 35, dpetal5)
-// circle (45, 50, dpetal5)
-// circle (26, 50, dpetal5)
-
-// flower 2 petals 
-// circle (160, 10, dpetal2)
-// circle (140, 20, dpetal2)
-// circle (145, 40, dpetal2)
-// circle (165, 45, dpetal2)
-// circle (175, 26, dpetal2)
-
-//new flower 2 
-// circle (155, 20, dpetal2)
-// circle (148, 30, dpetal2)
-// circle (155, 43, dpetal2)
-// circle (170, 43, dpetal2)
-// circle (170, 20, dpetal2)
-// circle (176, 30, dpetal2)
--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+const { switcher, rectangle, dpetal1, dflowercenter1, dpetal2, dflowercenter2, dpetal3, dflowercenter3, dpetal4, dflowercenter4, dpetal5, dflowercenter5, dpetal6, dflowercenter6 } = require("./my_wallpaper");
+
 let size = 50 
 
 //setup - run once when the code is first starts
@@ -26,6 +28,144 @@ function draw() {
 
 
  } 
+function my_symbol() {
+  //rect(40 ,40, rect_width, rect_height);
+  //og d petal 35 
+
+
+  if (switcher == true) {
+    rectangle();
+
+  }
+  noStroke(); // no outline 
+
+
+
+  //FLOWER 1 
+  //shadow
+  fill(196, 26, 139);
+  circle(64, 89, dpetal1);
+  circle(76, 63, dpetal1);
+  circle(106, 58, dpetal1);
+  circle(83, 110, dpetal1);
+
+  //petals 
+  fill(255, 39, 183);
+  circle(70, 90, dpetal1);
+  circle(80, 65, dpetal1);
+  circle(110, 60, dpetal1);
+  circle(125, 85, dpetal1);
+  circle(115, 110, dpetal1);
+  circle(87, 110, dpetal1);
+
+  //center of flower
+  fill(255, 245, 133);
+  circle(100, 85, dflowercenter1);
+
+  // FLOWER 2 
+  //shadow 
+  fill(214, 123, 26);
+  circle(156, 9, dpetal2);
+  circle(137, 19, dpetal2);
+  circle(142, 39, dpetal2);
+  //petals 
+  fill(255, 147, 31);
+  circle(160, 10, dpetal2);
+  circle(140, 20, dpetal2);
+  circle(145, 40, dpetal2);
+  circle(165, 45, dpetal2);
+  circle(175, 26, dpetal2);
+
+  //center 
+  fill(97, 227, 255);
+  circle(158, 28, dflowercenter2);
+
+  //FLOWER 3 
+  //shadow
+  fill(152, 191, 46);
+  circle(135, 154, dpetal3);
+  circle(146, 174, dpetal3);
+  circle(147, 134, dpetal3);
+  circle(170, 134, dpetal3);
+
+  //petals
+  fill(188, 234, 58);
+  circle(140, 155, dpetal3);
+  circle(152, 175, dpetal3);
+  circle(175, 175, dpetal3);
+  circle(185, 155, dpetal3);
+  circle(152, 135, dpetal3);
+  circle(175, 135, dpetal3);
+
+  //center 
+  fill(255, 77, 7);
+  circle(164, 155, dflowercenter3);
+
+  //FLOWER 4 
+  //shadow 
+  fill(179, 35, 30);
+  circle(27, 164, dpetal4);
+  circle(32, 174, dpetal4);
+  circle(32, 156, dpetal4);
+  circle(43, 156, dpetal4);
+
+  //petals
+  fill(255, 77, 77);
+  circle(30, 165, dpetal4);
+  circle(35, 175, dpetal4);
+  circle(45, 175, dpetal4);
+  circle(50, 165, dpetal4);
+  circle(45, 157, dpetal4);
+  circle(35, 157, dpetal4);
+
+  //center 
+  fill(188, 234, 58);
+  circle(40, 165, dflowercenter4);
+
+  //FLOWER 5 
+  //shadow 
+  fill(96, 162, 168);
+  circle(25, 24, dpetal5);
+  circle(31, 14, dpetal5);
+  circle(43, 14, dpetal5);
+  circle(30, 34, dpetal5);
+
+  //petals
+  fill(97, 227, 255);
+  circle(27, 25, dpetal5);
+  circle(33, 15, dpetal5);
+  circle(45, 15, dpetal5);
+  circle(50, 25, dpetal5);
+  circle(33, 35, dpetal5);
+  circle(45, 35, dpetal5);
+
+  //center 
+  fill(255, 151, 80);
+  circle(39, 25, dflowercenter5);
+
+  //FLOWER 6 
+  //shadow 
+  fill(95, 39, 143);
+  circle(-3, 109, dpetal6);
+  circle(-3, 120, dpetal6);
+  circle(8, 103, dpetal6);
+  circle(7, 124, dpetal6);
+
+  // //petals 
+  fill(141, 56, 214);
+  circle(0, 110, dpetal6);
+  circle(10, 105, dpetal6);
+  circle(18, 110, dpetal6);
+  circle(18, 120, dpetal6);
+  circle(10, 125, dpetal6);
+  circle(0, 120, dpetal6);
+
+  // //center
+  fill(255, 237, 57);
+  circle(9, 115, dflowercenter6);
+
+
+}
 
   // fill(252, 187, 239)
   // circle (50, 90, 25)
